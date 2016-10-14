6cb426700365c69742b3ef9731daf3952bf9f95c
Andy Zhao <andyzhao.zhao@gmail.com>
2016-10-14 11:42:37 -0400

    debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,9 @@
+class NoCompression
+  def compress(string)
+    string
+  end
+end
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -25,7 +31,8 @@ Rails.application.configure do
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  # config.assets.js_compressor = :uglifier
+  config.assets.compress = true
+  config.assets.js_compressor = NoCompression.new
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
